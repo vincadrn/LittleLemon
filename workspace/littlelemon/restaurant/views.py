bdda1d626b3181f3f5aec35fa6b4a9cda467d973
Vincentius Adrian <wsamudro@gmail.com>
2023-03-20 20:27:50 +0700

    Fix: Home page and static files

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index2.html', {})
 
 
 class MenuItemView(generics.ListCreateAPIView):
